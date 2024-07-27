68e435abcaf54ef1b79401431c60b5467ab26da6
huynhdong0 <huynhdongvcb1020@gmail.com>
2024-07-27 18:27:17 +0700

    setState

--- src/components/MyCompoent.js
+++ src/components/MyCompoent.js
@@ -2,6 +2,7 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    names = ['HD', 'Huynh Dong', 'Van Dong', 'Dong', 'Dong Huynh'];
     state = {
         name : "Huynh Dong",
         age: 21,
@@ -10,25 +11,31 @@ class MyComponent extends React.Component {
 
     handleClick() {
         console.log('>> Click me by button')
+        console.log(`My name is ${this.state.name}`)
+
+        this.setState({
+            name: this.names[(Math.floor(Math.random()*3)+1)],
+            age: Math.floor((Math.random() * 100) +1)
+        })
     }
 
-    render() {
+
+    render() {  
         return (
             <div>
-            <div>
-              My name is {this.state.name}
+                <div>
+                My name is {this.state.name}
+                </div>
+                <div>
+                I'm {this.state.age} years old
+                </div>
+                <div>
+                I'm from {this.state.address}
+                </div>
+                <button onClick={(event) => {this.handleClick(event)}}> Click me</button>
             </div>
-            <div>
-              I'm {this.state.age} years old
-            </div>
-            <div>
-              I'm from {this.state.address}
-            </div>
-    
-            <button onClick={this.handleClick}> Click me</button>
-          </div>
         );
-    }
+    };
 }
 
-export default MyComponent;
+export default MyComponent; 
