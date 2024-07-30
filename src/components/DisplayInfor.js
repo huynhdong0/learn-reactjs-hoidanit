271d580559cbce19f22b140e07ae0e7b122005e6
huynhdong0 <huynhdongvcb1020@gmail.com>
2024-07-30 16:02:30 +0700

    using functional component to replace class component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,33 +1,58 @@
 import React from "react";
-import './DisplayInfo.scss';
-import logo from '../logo.svg';
-class DisplayInfor extends React.Component {
+import './DisplayInfor.scss';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isShowHide: true
-        };
-    }
+// class DisplayInfor extends React.Component {
 
-    handleShowHide = () => {
-        this.setState({
-            isShowHide: !this.state.isShowHide
-        })
-    }
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             isShowHide: true
+//         };
+//     }
 
-    render() {
-        const {listUsers, handleDeleteUser} = this.props;
-        return(
-            <div className="display-infor-container">
+//     handleShowHide = () => {
+//         this.setState({
+//             isShowHide: !this.state.isShowHide
+//         })
+//     }
+
+//     render() {
+//         const {listUsers, handleDeleteUser} = this.props;
+//         return(
+//             <div className="display-infor-container">
+//                 <div>
+//                     <span  onClick={() => {this.handleShowHide()}}>
+//                         {this.state.isShowHide ? 'Hide' : 'Show'} list users
+//                     </span>
+//                 </div>
+//                 {/* <img src={logo} alt="logo"/> */}
+//                 {this.state.isShowHide && 
+//                     <div>
+//                     {listUsers.map((user) => {
+//                         return (
+//                             <div key={user.id} className={+user.age > 18 ? 'red' : 'green'}>
+//                                 <div>My name's: {user.name}</div>
+//                                 <div>My age's: {user.age}</div>
+//                                 <div>My address's: {user.address}</div>
+//                                 <div>
+//                                 <button onClick={() => {handleDeleteUser(user.id)}}>Delete</button>
+//                                 </div>
+//                                 <hr/>
+//                             </div>
+//                         );
+//                     })}
+//                     </div>}
+//             </div>
+//         );
+//     };
+// }
+
+const DisplayInfor = (props) => {
+    const {listUsers, handleDeleteUser} = props; // object
+    return(
+        <div className="display-infor-container">
+            {true && 
                 <div>
-                    <span  onClick={() => {this.handleShowHide()}}>
-                        {this.state.isShowHide ? 'Hide' : 'Show'} list users
-                    </span>
-                </div>
-                {/* <img src={logo} alt="logo"/> */}
-                {this.state.isShowHide && 
-                    <div>
                     {listUsers.map((user) => {
                         return (
                             <div key={user.id} className={+user.age > 18 ? 'red' : 'green'}>
@@ -41,9 +66,9 @@ class DisplayInfor extends React.Component {
                             </div>
                         );
                     })}
-                    </div>}
-            </div>
-        );
-    };
-}
+                </div>}
+        </div>
+    );
+};
+
 export default DisplayInfor;
