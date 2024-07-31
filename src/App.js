b3e8c552d434d2570021e98076020387def34228
huynhdong0 <huynhdongvcb1020@gmail.com>
2024-07-31 16:42:09 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import {Link} from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app-container'>
         <Header/>
+        <div>
+          <button>
+            <Link to="/users">go to to user page</Link>
+          </button>
+
+          <button>
+          <Link to="/admins">go to to user page</Link>
+          </button>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Admin from './components/Admin/Admin';
+import User from './components/User/User'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='users' element={<User/>}/>
+        <Route path='admins' element={<Admin/>}/>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
