e380c2bdc6daaf7e9637da42a9e40a1d4e3b3cb0
huynhdong0 <huynhdongvcb1020@gmail.com>
2024-07-31 15:54:13 +0700

    change git branch and add navbar for page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className='app-container'>
-        Hell world!
+        <Header/>
     </div>
   );
 }
