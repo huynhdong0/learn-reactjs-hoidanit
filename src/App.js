958a9f00323878fe58a16f565e6a44a0812d0f62
huynhdong0 <huynhdongvcb1020@gmail.com>
2024-07-31 17:18:05 +0700

    navlink and nested route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.scss';
 import Header from './components/Header/Header';
-import {Link} from "react-router-dom";
+import { Outlet} from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app-container'>
-        <Header/>
-        <div>
-          <button>
-            <Link to="/users">go to to user page</Link>
-          </button>
-
-          <button>
-          <Link to="/admins">go to to user page</Link>
-          </button>
+        <div className='header-container'>
+          <Header/>
+        </div>
+        <div className='main-container'>
+          <div className='sidebar-container'></div>
+          <div className='app-context'>
+            <Outlet/>
+          </div>
         </div>
     </div>
   );
